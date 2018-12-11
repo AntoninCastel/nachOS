082d857d20bc6428c98d2560d643a949ee9e789f
Eymond Laritaz <cyprien@lambdalab.fr>
2018-12-11 13:17:36 +0100

    Documentation adaptée à doxygen de addrspace.h

--- code/userprog/addrspace.h
+++ code/userprog/addrspace.h
@@ -1,14 +1,18 @@
-// addrspace.h 
-//      Data structures to keep track of executing user programs 
-//      (address spaces).
-//
-//      For now, we don't keep any information about address spaces.
-//      The user level CPU state is saved and restored in the thread
-//      executing the user program (see thread.h).
-//
-// Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
-// of liability and disclaimer of warranty provisions.
+/**
+ * \file addrspace.h
+ * \brief Data structures to keep track of executing user programs 
+ * (address spaces).
+ *
+ * Data structures to keep track of executing user programs 
+ * (address spaces).
+ * For now, we don't keep any information about address spaces.
+ * The user level CPU state is saved and restored in the thread
+ * executing the user program (see thread.h).
+ * 
+ * \copyright Copyright (c) 1992-1993 The Regents of the University of California. 
+ * All rights reserved.  See copyright.h for copyright notice and limitation 
+ * of liability and disclaimer of warranty provisions.
+ */
 
 #ifndef ADDRSPACE_H
 #define ADDRSPACE_H
@@ -16,27 +20,42 @@
 #include "copyright.h"
 #include "filesys.h"
 
-#define UserStackSize		1024	// increase this as necessary!
+/// increase this as necessary !
+#define UserStackSize		1024
 
+/**
+ * \class AddrSpace addrspace.h
+ */
 class AddrSpace
 {
   public:
-    AddrSpace (OpenFile * executable);	// Create an address space,
-    // initializing it with the program
-    // stored in the file "executable"
-    ~AddrSpace ();		// De-allocate an address space
+    /**
+    * \brief Create an address space, initializing it with the program 
+    * stored in the file "executable"
+    */
+    AddrSpace (OpenFile * executable); 
 
-    void InitRegisters ();	// Initialize user-level CPU registers,
-    // before jumping to user code
+    /// De-allocate an address space
+    ~AddrSpace ();
 
-    void SaveState ();		// Save/restore address space-specific
-    void RestoreState ();	// info on a context switch 
+    /**
+     * \brief Initialize user-level CPU registers,
+     * before jumping to user code
+     */
+    void InitRegisters ();
+
+    /// Save address space-specific info on a context switch 
+    void SaveState ();
+    
+    /// Restore address space-specific info on a context switch 
+    void RestoreState (); 
 
   private:
-      TranslationEntry * pageTable;	// Assume linear page table translation
-    // for now!
-    unsigned int numPages;	// Number of pages in the virtual 
-    // address space
+    /// Assume linear page table translation for now !
+    TranslationEntry * pageTable; 
+
+    /// Number of pages in the virtual address space
+    unsigned int numPages; 
 };
 
 #endif // ADDRSPACE_H

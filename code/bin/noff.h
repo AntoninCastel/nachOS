4e836a14cb2a133945425b132d50076f2f85a068
Eymond Laritaz <cyprien@lambdalab.fr>
2018-12-11 14:49:04 +0100

    Format de documentation pour noff.h adapté à doxygen

--- code/bin/noff.h
+++ code/bin/noff.h
@@ -1,25 +1,46 @@
-/* noff.h 
- *     Data structures defining the Nachos Object Code Format
+/**
+ * \file noff.h 
+ * \brief Data structures defining the Nachos Object Code Format
  *
- *     Basically, we only know about three types of segments:
+ * Data structures defining the Nachos Object Code Format
+ *
+ *  Basically, we only know about three types of segments:
  *	code (read-only), initialized data, and unitialized data
  */
 
-#define NOFFMAGIC	0xbadfad 	/* magic number denoting Nachos 
-					 * object code file 
-					 */
+/// magic number denoting Nachos object code file
+#define NOFFMAGIC	0xbadfad 	
 
+/**
+ * \struct segment
+ */
 typedef struct segment {
-  int virtualAddr;		/* location of segment in virt addr space */
-  int inFileAddr;		/* location of segment in this file */
-  int size;			/* size of segment */
+  /// location of segment in virt addr space
+  int virtualAddr;
+  
+  /// location of segment in this file
+  int inFileAddr;
+
+  /// size of segment
+  int size;			
 } Segment;
 
+/**
+ * \struct noffHeader
+ */
 typedef struct noffHeader {
-   int noffMagic;		/* should be NOFFMAGIC */
-   Segment code;		/* executable code segment */ 
-   Segment initData;		/* initialized data segment */
-   Segment uninitData;		/* uninitialized data segment --
-				 * should be zero'ed before use 
-				 */
+   /// should be NOFFMAGIC
+   int noffMagic;
+
+   /// executable code segment
+   Segment code;
+
+   /// initialized data segment
+   Segment initData;
+
+   /**
+    * \brief uninitialized data segment --
+    * should be zero'ed before use 
+    */
+   Segment uninitData;
 } NoffHeader;

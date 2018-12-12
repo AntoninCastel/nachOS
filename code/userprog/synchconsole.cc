1f064f9266c8fea21e59b68dfd5a8d1552a36752
Eymond Laritaz <cyprien@lambdalab.fr>
2018-12-12 15:20:44 +0100

    Version indentée du squelette synchconsole

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -2,36 +2,42 @@
 #include "system.h"
 #include "synchconsole.h"
 #include "synch.h"
+
 static Semaphore *readAvail;
 static Semaphore *writeDone;
 static void ReadAvail(int arg) { readAvail->V(); }
 static void WriteDone(int arg) { writeDone->V(); }
-3
+
 SynchConsole::SynchConsole(char *readFile, char *writeFile)
 {
-readAvail = new Semaphore("read avail", 0);
-writeDone = new Semaphore("write done", 0);
-// console = ...
+    readAvail = new Semaphore("read avail", 0);
+    writeDone = new Semaphore("write done", 0);
+    // console = ...
 }
+
 SynchConsole::~SynchConsole()
 {
-delete console;
-delete writeDone;
-delete readAvail;
+    delete console;
+    delete writeDone;
+    delete readAvail;
 }
+
 void SynchConsole::SynchPutChar(const char ch)
 {
-// ...
+    // ...
 }
+
 char SynchConsole::SynchGetChar()
 {
-// ...
+    // ...
 }
+
 void SynchConsole::SynchPutString(const char s[])
 {
-// ...
+    // ...
 }
+
 void SynchConsole::SynchGetString(char *s, int n)
 {
-// ...
+    // ...
 }

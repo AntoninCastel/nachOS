ca9655d70cbf075fe4ba84b392bc371200a9e98c
Marianne <marianne.garnier@etu.univ-grenoble-alpes.fr>
2018-12-13 10:12:17 +0100

    màj syscall.h : ajout de PutChar

--- code/userprog/syscall.h
+++ code/userprog/syscall.h
@@ -33,6 +33,7 @@
 #define SC_Close	8
 #define SC_Fork		9
 #define SC_Yield	10
+#define SC_PutChar	11
 
 
 #ifdef IN_USER_MODE
@@ -142,6 +143,14 @@ void Fork (void (*func) ());
  * or not. 
  */
 void Yield ();
+
+
+/**
+ *  Takes as an argument a character c in user mode, and then sets
+ *  SyscallException interrupt. This causes a switch to kernel mode and the
+ *  execution of the standard handle.
+ */
+void PutChar(char c);
 
 
 #endif // IN_USER_MODE

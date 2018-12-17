1870e73a8d4a289c1392768549da99a883f85636
Antonin <castelantonin03@gmail.com>
2018-12-17 23:12:18 +0100

    modification de StartUserThread pour prendre en compte le SP du thread appelant

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -6,14 +6,19 @@ static void StartUserThread(int f);
 
 int do_UserThreadCreate(int f, int arg) {
 	param_t *p = new param_t;
-	*p = {.fn = f, .param = arg};
+	*p = {.fn = f, .param = arg, .SP = machine->ReadRegister (29)};
 	Thread *newthread = new Thread("new Thread");
 	newthread->Fork(StartUserThread, (int)p);
 	return -1; // non definitif
 }
 
 static void StartUserThread(int f) {
-	//param_t* p = (param_t*)f;
-	currentThread->space->InitRegisters();
-	machine->Run();
+	/*param_t* p = (param_t*)f;
+	for (int i = 0; i < NumTotalRegs; i++){
+		machine->WriteRegister (i, 0);
+	}
+    machine->WriteRegister (PCReg, p->fn);
+    machine->WriteRegister (NextPCReg, p->fn+4);
+    machine->WriteRegister (StackReg, p->SP - 3*PageSize );
+	machine->Run();*/
 }
--- code/userprog/userthread.h
+++ code/userprog/userthread.h
@@ -4,6 +4,7 @@
 typedef struct {
 	int fn;
 	int param;
+	int SP;
 } param_t;
 
 extern int do_UserThreadCreate(int f, int arg);
